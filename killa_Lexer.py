a27b613f4ec5d026fe71a3c2e061dc922e62a239
daidai0000 <daiandrew0107@gmail.com>
2025-04-10 14:29:48 +0800

    新增特殊語法

--- killa_Lexer.py
+++ killa_Lexer.py
@@ -6,12 +6,28 @@ class Lexer:
         # 定義 Token 類型 dict型別(用於特殊符號)
         self.reserved = {
             'id': 'ID',
+            'for': 'FOR',
+            'while': 'WHILE',
             'if': 'IF',
             'else': 'ELSE',
-            'equal': 'EQUAL'
+            'brk': 'BREAK',
+            'in': 'IN',
+            'range': 'RANGE',
+            'prt': 'PRINT',
+            'ret': 'RETURN',
+            'switch': 'SWITCH',
+            'case': 'CASE',
+            'default': 'DEFAULT',
+            'var': 'VAR',
+            'func': 'FUNC',
         }
         self.tokens = (
             'NUMBER',  # 數字
+            'GREATER_THEN_EQUAL',  # >=
+            'GREATER_THEN',  # >
+            'LESS_THEN_EQUAL',  # <
+            'LESS_THEN',  # <=
+            'PLUSPLUS',  # ++
             'PLUS',  # +
             'MINUS',  # -
             'TIMES',  # *
@@ -19,18 +35,25 @@ class Lexer:
             'LPAREN',  # (
             'RPAREN',  # )
             'DIVISIBILITY',  # //
-            'PLUSPLUS', # ++
+            'EQUAL',  # =
+            'dot', # ,
         ) + tuple(self.reserved.values())  # 轉換成tuple
 
         # 定義 Token 規則（t_ 開頭）
         self.t_PLUS = r'\+'
-        self.t_MINUS = r'-'
+        self.t_MINUS = r'\-'
         self.t_TIMES = r'\*'
         self.t_DIVISION = r'/'
         self.t_LPAREN = r'\('
         self.t_RPAREN = r'\)'
-        self.t_DIVISIBILITY = r'//'
-        self.t_PLUSPLUS = r'\++'
+        self.t_DIVISIBILITY = r'\/\/'
+        self.t_EQUAL = r'='
+        self.t_PLUSPLUS = r'\+\+'
+        self.t_GREATER_THEN_EQUAL = r'>='
+        self.t_GREATER_THEN = r'>'
+        self.t_LESS_THEN_EQUAL = r'<='
+        self.t_LESS_THEN = r'<'
+        self.t_dot = r','
         # 忽略空格
         self.t_ignore = ' \t'
         # 建立 Lexer
@@ -60,10 +83,6 @@ class Lexer:
         r'\n+'
         t.lexer.lineno += len(t.value)
 
-    def t_EQUAL(self, t):
-        r'='
-        t.value = self.reserved.get(t.value, 'EQUAL')
-        return t
     # 取得 Token
     def token(self):
         return self.lexer.token()
@@ -72,7 +91,8 @@ class Lexer:
 # test Lexer
 if __name__ == "__main__":
     lexer = Lexer()
-    data = "1+1"
+    data = ("var x,"
+            "var y = 10,")
     lexer.input(data)
     while True:
         token = lexer.token()
--- killa_Parser.py
+++ killa_Parser.py
@@ -6,12 +6,20 @@ tokens = lexer.tokens  # 取得 tokens
 variables = {}  # 用來存放變數的字典
 
 
-# 運算表達式
+# 表達式或賦值
+def p_statement(p):
+    '''statement : assignment
+                | expression'''
+    p[0] = p[1]
+
+
+# 加法
 def p_expression_plus(p):
     'expression : expression PLUS term'
     p[0] = p[1] + p[3]
 
 
+# 減法
 def p_expression_minus(p):
     'expression : expression MINUS term'
     p[0] = p[1] - p[3]
@@ -22,12 +30,13 @@ def p_expression_term(p):
     p[0] = p[1]
 
 
-# 乘法 / 除法
+# 乘法
 def p_term_times(p):
     'term : term TIMES factor'
     p[0] = p[1] * p[3]
 
 
+# 除法
 def p_term_div(p):
     'term : term DIVISION factor'
     p[0] = p[1] / p[3]
@@ -44,48 +53,66 @@ def p_factor_num(p):
     p[0] = p[1]
 
 
+# 括號
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+
 # 處理變數 ID
 def p_factor_id(p):
     'factor : ID'
     p[0] = variables.get(p[1], 0)  # 取得變數值，若不存在則預設為 0
 
 
-# 括號運算
-def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
-    p[0] = p[2]
-
-
-# 賦值語句
+# 賦值語法
 def p_assignment_assign(p):
-    'statement : ID EQUAL expression'
+    'assignment : VAR ID EQUAL expression'
     variables[p[1]] = p[3]
     p[0] = p[3]
 
 
-# 表達式或賦值
-def p_statement_expr(p):
-    'statement : expression'
-    p[0] = p[1]
+# if-else statement
+def p_statement_if(p):
+    'statement : IF LPAREN expression RPAREN statement ELSE statement'
+    if p[3]:  # 如果條件為 True，執行 if 區塊
+        p[0] = p[5]
+    else:  # 否則執行 else 區塊
+        p[0] = p[7]
 
 
-# 錯誤處理
+# 整數除法
+def p_term_divisibility(p):
+    'term : term DIVISIBILITY factor'
+    p[0] = p[1] // p[3]
+
+
+def p_term_dot(p):
+    'term : term dot factor'
+    p[0] = p[1] + p[3]
+
+
+def p_statement_print(p):
+    'statement : PRINT LPAREN expression RPAREN'
+    print(p[3])
+    p[0] = p[3]
+
+
+# Error code
 def p_error(p):
-    if p:
-        print(f"Syntax Erro: {p.value}")
-    else:
-        print("語法錯誤！")
-
-def p_factor_id(p):
-    'factor : ID'
-    p[0] = variables.get(p[1], 0)
+    print(f"Syntax Error: {p.value}")
 
 
+def p_assignment_error(p):
+    'assignment : ID EQUAL expression'
+    print("Syntax Error: Use 'var' to declare variables.")
+    p[0] = None
 
-# 建立 Parser
+
+# build Parser
 parser = yacc.yacc()
 
-# 讀取使用者輸入
+# read input
 if __name__ == "__main__":
     while True:
         try:
